47a948821f3d995f33aba383cb8209ea764e8abe
ZYSzys <17367077526@163.com>
2018-09-23 20:46:17 +0800

    remove vue-resource

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import VueResource from 'vue-resource';
 
-Vue.use(VueResource);
 Vue.config.productionTip = false
 
 new Vue({
